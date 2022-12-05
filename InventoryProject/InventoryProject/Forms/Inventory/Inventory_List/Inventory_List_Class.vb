6850dc82e23873c6fed187438423ec9303cc4d99
floydferd <francethyst24@gmail.com>
2022-12-05 11:09:09 +0800

    File Hierarchy

---
+++ InventoryProject/InventoryProject/Forms/Inventory/Inventory_List/Inventory_List_Class.vb
@@ -0,0 +1,3 @@
+﻿Public Class Inventory_List_Class
+
+End Class
--- InventoryProject/InventoryProject/My Project/Application.Designer.vb
+++ InventoryProject/InventoryProject/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.InventoryProject.Form1
+            Me.MainForm = Global.InventoryProject.Inventory_List
         End Sub
     End Class
 End Namespace
